dcd41f90bb5381b2ec1fca32277a59224af964a9
Betül <betulyilmaz670@gmail.com>
2026-03-11 22:04:38 +0300

    güncellendi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
   export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://ygaxylggrrznwgyjzwem.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnYXh5bGdncnJ6bndneWp6d2VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4Mjk1NTcsImV4cCI6MjA4MTQwNTU1N30.6Kx8FRCmv3VdavgNk7MV7WL_2ifitmw03gvyPVxzn4w'
 };
